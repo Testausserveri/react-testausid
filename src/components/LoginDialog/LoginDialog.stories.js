6ba72d25959b4435be3189d58584e98988999ed8
Esinko <34211401+Esinko@users.noreply.github.com>
2022-12-16 11:29:19 +0200

    chore: change some storybook settings

--- src/components/LoginDialog/LoginDialog.stories.js
+++ src/components/LoginDialog/LoginDialog.stories.js
@@ -17,5 +17,6 @@ Primary.args = {
       'https://cdn.discordapp.com/avatars/746084561062068345/fa4ccd88f599bc6b890c8db7528f64b6.webp?width=702&height=702'
   },
   scopes: ['id', 'account', 'contact', 'security'],
-  client: '181937620043556561658238287560538816854' // Add your client ID here!
+  client: '181937620043556561658238287560538816854', // Add your client ID here!
+  onlyToken: true // Do you want just the token or the user data?
 }
